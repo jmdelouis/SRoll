7fc87250badc0467f3c3a9f7d35bdd45400b201c
TF22559 <45318649+TF22559@users.noreply.github.com>
2023-04-12 12:03:38 +0200

    Clean and update unit test

--- sroll4/unit_test_857.py
+++ sroll4/unit_test_857.py
@@ -5,17 +5,11 @@ def main():
  
   nbolo = 4
   bolo=['857-1','857-2','857-3','857-4']
-  bolo2= ['25_857_1','35_857_2','65_857_3','74_857_4'] # ID for badring
+  bolo2= ['25_857_1','35_857_2','65_857_3','74_857_4']
   BeginRing = 0
   EndRing= 499
   RSTEP = 100
-
-  beg_surv=[240,240,13144,240,5720,11194,16691,21720]
-  end_surv=[26050,13145,26050,5721,11195,16692,21721,27005]
-  name_surv=['Full','hm1','hm2','s1','s2','s3','s4','s5']
-  
-  n_bolo =len(bolo)
-
+ 
   NORM_GAIN = 1
   REMOVE_CAL = 1
 
@@ -31,7 +25,7 @@ def main():
   
   XI2STOP = 1.0
   seuilcond = 100.0
-  NITT = 10
+  NITT = 2
   verbose = 0
   TEMPLATE_NSIDE = 128
   BUILDTF = 0
@@ -42,10 +36,9 @@ def main():
   NADU = [16,16,16,16]
   NADUSTEP= [1,1,1,1]
 
-  ##################### CNN ################################
-  #PARAMETRES RELIE AU RESEAU DE NEURONE
+
+  ################################## #PARAMETRES RELIE AU RESEAU DE NEURONE  ################################## 
   DOCNN = [0,0,0,1,1,1]
-  #DOCNN = [0,0,0,0,0,0,0,0,0,0,0,0,0]  
 
   CALLCNN = "/home3/homedir7/perso/tfoulqui/workspace/srollexx_work/sroll/py_function_pwst_pol.py"
 
@@ -56,16 +49,12 @@ def main():
   CNN_RESIDU = 0.0
   CNN_WEIGHTS = "/export/home/tfoulquier/WIDXR_files"
 
-  N_IN_ITT = 300
+  N_IN_ITT = 100
 
   #Path for netcdf
   INST_CNN = '/home3/homedir7/perso/tfoulqui/workspace/srollexx_work/sroll'
   MAP_CNN =  '/home3/homedir7/perso/tfoulqui/workspace/srollexx_work/sroll'
-
-  #############################################################
-  
-  
-  #Calibration = [3.30076826046e-16,3.55811287601e-16,3.18681631353e-16,2.219187708e-16]
+  ############################################################################################################ 
   
   Calibration = [1.0,1.0,1.0,1.0]  
   CrossPol = [0.0,0.0,0.0,0.0]
@@ -74,32 +63,25 @@ def main():
   FSLCOEF = [0.0,0.0,0.0,0.0]
   OUT_NOPOL = [1,1,1,1,1]
   n_OUT_NOPOL=len(OUT_NOPOL)
-
-  # bolomask
+   # bolomask
   bolomask = [1,1,1,1,
               1,0,0,1,
               0,1,1,0]
   
-  MAPRINGS = [1]
 
-  ##**##
   ############################################## INPUTS ############################################################
   
   Mask = "/home1/scratch/jmdeloui/DATA4SROLL4/mask_RD_857"
   TEMPLATEMAP = "/export/home/tfoulquier/data_sroll/MAP/map_857_2018.float32.bin"
   
-  projectionType = "I" #"I,Q,U"
+  projectionType = "I"
 
-  in_template_map_I = ["/export/home/tfoulquier/data_sroll/MAP/map_857_2018.float32.bin" for i in range(nbolo)]
-  
-  in_template_map_Q = ["/export/home/tfoulquier/data_sroll/MAP/map_null.float32.bin"for i in range(0,nbolo)]
-  
-  in_template_map_U = ["/export/home/tfoulquier/data_sroll/MAP/map_null.float32.bin"for i in range(0,nbolo)]
-  
+  in_template_map = ["/export/home1/jmdeloui/DATA4SROLL4/map_857_2018.float32.bin" for i in range(nbolo)]
+
   Signal_noPS =  ["/export/home1/jmdeloui/DATA4SROLL4/%s_REP7_2"%(i) for i in bolo]
-    
+ 
   ADU = ["/export/home1/jmdeloui/DATA4SROLL4/%s_REP6_phregul"%(i) for i in bolo]
-  
+
   Badring = ["/export/home1/jmdeloui/DATA4SROLL4/%s_discarded_rings_dx11"%(i) for i in bolo2]
 
   HPR_Calib = ["/export/home1/jmdeloui/DATA4SROLL4/%s_REP6_diporb_quat"%(i) for i in bolo]  
@@ -107,20 +89,7 @@ def main():
   Hit_noPS = ["/export/home1/jmdeloui/DATA4SROLL4/%s_REP6_hit"%(i) for i in bolo]
   
   Ptg_noPS = ["/export/home1/jmdeloui/DATA4SROLL4/%s_REP6_ptg"%(i) for i in bolo]
-
-  OMAP = '3'
-  #do_mean = 0 #if = 1 do moyennne des bolometre =0 dans projgrad2 dans troll.c // tableau taille nbolo
-  val_mean = [0.0 for i in range(4)]
   
-  w_mean = [1E8 for i in range(4)]
-  
-  nspline = 16
-  do_mean  = []
-  # mean of H0
-  for j in range(4):
-    do_mean=do_mean+[int((i%4)==j) for i in range(4*nspline)]   
-
-
   Theo_MAP = ['/export/home1/jmdeloui/DATA4SROLL4/NEW_cleaned_12CO.float32.bin',
               '/export/home1/jmdeloui/DATA4SROLL4/FREE_FREE_mod.float32.bin',
               '/export/home1/jmdeloui/DATA4SROLL4/Dust_New_I']
@@ -133,24 +102,42 @@ def main():
   phase = ["/export/home1/jmdeloui/DATA4SROLL4/%s_REP6_phregul"%(i) for i in bolo]
   rgcnn = ["/export/home1/jmdeloui/DATA4SROLL4/%s_REP6_rgadutot.int32.bin"%(i) for i in bolo]
 
+ 
+  #do_mean = 0 #if = 1 do moyennne des bolometre =0 dans projgrad2 dans troll.c // tableau taille nbolo
+  val_mean = [0.0 for i in range(4)]  
+  w_mean = [1E8 for i in range(4)]
+  do_mean  = []
+  nb_mean = 7
+  # mean of H0
+  for j in range(4):
+    do_mean=do_mean+[int((i%4)==j) for i in range(4*nb_mean)]   
+
+
+  #output params : len(MAPRINGS) == len(bolomask)
+  # Defini le nombre de carte de sortie en fonctions des survey
+  MAPRINGS = [3,2,1,1,1]
+
+  beg_surv=[0,0,128]
+  end_surv=[499,128,499]
+  name_surv=['Full','hm1','hm2']
 
 
 
   ####################################### OUTPUTS  ##################################################################
-  bolo_map = ['857GHz']
+  bolo_map = ['857GHz','857-1','857-2','857-3','857-4']
 
-  Out_MAP = 
-  Out_VEC = 
-  Out_Offset = 
-  Out_Offset_corr = 
-  Out_xi2 = 
-  Out_xi2_corr = 
+  Out_MAP = ["/export/home/tfoulquier/workspace/SROLL/MAP/857_maps/nside%s_%s"%(Nside,i) for i in bolo_map]
+  Out_VEC = ["/export/home/tfoulquier/workspace/SROLL/VEC/857_vecs/nside_%s%s"%(Nside,i) for i in bolo]
+  Out_Offset = ["/export/home/tfoulquier/workspace/SROLL/VEC/857_vecs/nside%s_%s"%(Nside,i) for i in bolo]
+  Out_Offset_corr = ["/export/home/tfoulquier/workspace/SROLL/VEC/857_vecs/nside%s_%s"%(Nside,i) for i in bolo]
+  Out_xi2 = ["/export/home/tfoulquier/workspace/SROLL/VEC/857_vecs/nside%s_%s"%(Nside,i) for i in bolo]
+  Out_xi2_corr = ["/export/home/tfoulquier/workspace/SROLL/VEC/857_vecs/nside%s_%s"%(Nside,i) for i in bolo]
+
 
 
   params = vars()
 
   return params
-
 
 
 
